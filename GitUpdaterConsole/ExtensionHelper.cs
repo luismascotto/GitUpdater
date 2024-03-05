dd2c44593fdd4bce7283ad146e2051c2cf3143ed
Luis Mascotto <gustavomascotto@gmail.com>
2024-03-05 16:26:18 -0300

    - List remainig repositories count - More randomizes colours - Aggressive option to run git maintenance run --task gc

---
+++ GitUpdaterConsole/ExtensionHelper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitUpdaterConsole;
+internal static class ExtensionHelper
+{
+    public static string GetAppSetting(this IConfiguration config, string key, string defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key)
+    {
+        return config.GetAppSetting(key, string.Empty);
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, int defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, bool defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, double defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, float defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, decimal defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, long defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, short defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, byte defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, char defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, DateTime defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, TimeSpan defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetAppSetting(this IConfiguration config, string key, Guid defaultValue)
+    {
+        return string.IsNullOrEmpty(config[key]) ? defaultValue.ToString() : config[key]!;
+    }
+    public static string GetLastDirectory(this string path)
+    {
+        return path.Split(Path.PathSeparator).Last();
+    }
+}
--- GitUpdaterConsole/Program.cs
+++ GitUpdaterConsole/Program.cs
@@ -1,12 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CliWrap;
 using CliWrap.Buffered;
+using GitUpdaterConsole;
 using Microsoft.Extensions.Configuration;
 
 var switchMappings = new Dictionary<string, string>()
            {
                { "--path", "Path" },
                { "--parallellism", "MaxDegreeOfParallelism" },
+               { "--aggressive", "Aggressive" },
            };
 
 IConfiguration config = new ConfigurationBuilder()
@@ -24,6 +26,11 @@ if (string.IsNullOrEmpty(config["MaxDegreeOfParallelism"]))
 {
     config["MaxDegreeOfParallelism"] = "2";
 }
+
+if (string.IsNullOrEmpty(config["Aggressive"]))
+{
+    config["Aggressive"] = "false";
+}
 if (Directory.Exists(config["Path"]))
 {
     var gitDirs = new List<string>();
@@ -38,6 +45,12 @@ if (Directory.Exists(config["Path"]))
             //}
         }
     }
+    if (gitDirs.Count == 0)
+    {
+        Console.WriteLine("No git repositories found");
+        return;
+    }
+    int iCountRemaining = gitDirs.Count;
     Console.WriteLine($"Found {gitDirs.Count} git repositories");
 
     Parallel.ForEach(
@@ -46,6 +59,8 @@ if (Directory.Exists(config["Path"]))
         gitDir =>
         {
             CliUpdateGit(gitDir).Wait();
+            Interlocked.Decrement(ref iCountRemaining);
+            Console.WriteLine($" <-------  {iCountRemaining}     {gitDir.GetLastDirectory()}{Environment.NewLine}");
         });
     //foreach (var gitDir in gitDirs)
     //{
@@ -54,10 +69,25 @@ if (Directory.Exists(config["Path"]))
     //Console.ReadKey(true);
 }
 
-static async Task CliUpdateGit(string gitDir)
+async Task CliUpdateGit(string gitDir, int remaining = 0)
 {
     try
     {
+        Console.ForegroundColor = colorize();
+        if (bool.Parse(config["Aggressive"] ?? "false"))
+        {
+            var gitMaintenance = await Cli.Wrap("git")
+                .WithWorkingDirectory(gitDir)
+                .WithArguments(args => args
+                    .Add("maintenance")
+                    .Add("run")
+                    .Add("--task")
+                    .Add("gc")
+                )
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteBufferedAsync();
+            Console.WriteLine($"{gitDir} - ExitCode:{gitMaintenance.ExitCode} Output:{gitMaintenance.StandardOutput}{Environment.NewLine}{gitMaintenance.StandardError}");
+        }
         var result = await Cli.Wrap("git")
                 .WithWorkingDirectory(gitDir)
                 .WithArguments(args => args
@@ -70,10 +100,8 @@ static async Task CliUpdateGit(string gitDir)
                 )
                 .WithValidation(CommandResultValidation.None)
                 .ExecuteBufferedAsync();
-        Random.Shared.NextBytes(new byte[1]);
-        Console.ForegroundColor = colorize();
-        Console.WriteLine($"{gitDir} - ExitCode:{result.ExitCode} Output:{result.StandardOutput}");
-        Console.WriteLine($"{result.StandardError}");
+        Console.WriteLine($"{gitDir} - ExitCode:{result.ExitCode} Output:{result.StandardOutput}{Environment.NewLine}{result.StandardError}");
+
         Console.ResetColor();
     }
     catch (Exception ex)
@@ -88,14 +116,32 @@ static async Task CliUpdateGit(string gitDir)
 static ConsoleColor colorize()
 {
     List<ConsoleColor>? values = null;
+    //More chance for light colors
     values ??=
         [
-            ConsoleColor.White,
-            ConsoleColor.Green,
             ConsoleColor.Blue,
-            ConsoleColor.Yellow,
-            ConsoleColor.Gray,
             ConsoleColor.Cyan,
+            ConsoleColor.Gray,
+            ConsoleColor.Green,
+            ConsoleColor.Magenta,
+            ConsoleColor.Red,
+            ConsoleColor.White,
+            ConsoleColor.Yellow,
+            ConsoleColor.DarkBlue,
+            ConsoleColor.DarkCyan,
+            ConsoleColor.DarkGray,
+            ConsoleColor.DarkGreen,
+            ConsoleColor.DarkMagenta,
+            ConsoleColor.DarkRed,
+            ConsoleColor.DarkYellow,
+            ConsoleColor.Blue,
+            ConsoleColor.Cyan,
+            ConsoleColor.Gray,
+            ConsoleColor.Green,
+            ConsoleColor.Magenta,
+            ConsoleColor.Red,
+            ConsoleColor.White,
+            ConsoleColor.Yellow,
         ];
     return values[Random.Shared.Next(values.Count)];
 }
