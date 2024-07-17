7f78fc815febdafaa95fe81bc320a990162110ed
Luis Mascotto <gustavomascotto@gmail.com>
2024-07-17 11:05:13 -0300

    - Included Update Rust (rustup update stable)

--- GitUpdaterConsole/Program.cs
+++ GitUpdaterConsole/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CliWrap;
+﻿using CliWrap;
 using CliWrap.Buffered;
 using GitUpdaterConsole;
 using Microsoft.Extensions.Configuration;
@@ -8,7 +7,8 @@ var switchMappings = new Dictionary<string, string>()
            {
                { "--path", "Path" },
                { "--parallellism", "MaxDegreeOfParallelism" },
-               { "--aggressive", "Aggressive" },
+               //{ "--aggressive", "Aggressive" },
+               { "--rust", "UpdateRust" },
            };
 
 IConfiguration config = new ConfigurationBuilder()
@@ -17,32 +17,21 @@ IConfiguration config = new ConfigurationBuilder()
             .AddCommandLine(args, switchMappings)
             .Build();
 
-if (string.IsNullOrEmpty(config["Path"]))
-{
-    config["Path"] = @"K:\DesenvolvimentoGit";
-}
+string _Path = config["Path"] ?? @"K:\DesenvolvimentoGit";
+int _MaxDegreeOfParallelism = int.Parse(config["MaxDegreeOfParallelism"] ?? "2");
+bool _Aggressive = bool.Parse(config["Aggressive"] ?? "false");
+bool _UpdateRust = bool.Parse(config["UpdateRust"] ?? "false");
 
-if (string.IsNullOrEmpty(config["MaxDegreeOfParallelism"]))
-{
-    config["MaxDegreeOfParallelism"] = "2";
-}
 
-if (string.IsNullOrEmpty(config["Aggressive"]))
-{
-    config["Aggressive"] = "false";
-}
-if (Directory.Exists(config["Path"]))
+
+if (Directory.Exists(_Path))
 {
     var gitDirs = new List<string>();
-    foreach (var subdir in Directory.GetDirectories(config["path"]!))
+    foreach (var subdir in Directory.GetDirectories(_Path))
     {
         if (Directory.Exists(Path.Combine(subdir, ".git")))
         {
             gitDirs.Add(subdir);
-            //if (gitDirs.Count > 3)
-            //{
-            //    break;
-            //}
         }
     }
     if (gitDirs.Count == 0)
@@ -55,18 +44,18 @@ if (Directory.Exists(config["Path"]))
 
     Parallel.ForEach(
         gitDirs,
-        new ParallelOptions { MaxDegreeOfParallelism = int.Parse(config["MaxDegreeOfParallelism"] ?? "1") },
+        new ParallelOptions { MaxDegreeOfParallelism = _MaxDegreeOfParallelism },
         gitDir =>
         {
             CliUpdateGit(gitDir).Wait();
             Interlocked.Decrement(ref iCountRemaining);
             Console.WriteLine($" <-------  {iCountRemaining}     {gitDir.GetLastDirectory()}{Environment.NewLine}");
         });
-    //foreach (var gitDir in gitDirs)
-    //{
-    //    await CliUpdateGit(gitDir);
-    //}
-    //Console.ReadKey(true);
+
+    if (_UpdateRust)
+    {
+        CliUpdateRust().Wait();
+    }
 }
 
 async Task CliUpdateGit(string gitDir, int remaining = 0)
@@ -74,20 +63,20 @@ async Task CliUpdateGit(string gitDir, int remaining = 0)
     try
     {
         Console.ForegroundColor = colorize();
-        if (bool.Parse(config["Aggressive"] ?? "false"))
-        {
-            var gitMaintenance = await Cli.Wrap("git")
-                .WithWorkingDirectory(gitDir)
-                .WithArguments(args => args
-                    .Add("maintenance")
-                    .Add("run")
-                    .Add("--task")
-                    .Add("gc")
-                )
-                .WithValidation(CommandResultValidation.None)
-                .ExecuteBufferedAsync();
-            Console.WriteLine($"{gitDir} - ExitCode:{gitMaintenance.ExitCode} Output:{gitMaintenance.StandardOutput}{Environment.NewLine}{gitMaintenance.StandardError}");
-        }
+        //if (bool.Parse(config["Aggressive"] ?? "false"))
+        //{
+        //    var gitMaintenance = await Cli.Wrap("git")
+        //        .WithWorkingDirectory(gitDir)
+        //        .WithArguments(args => args
+        //            .Add("maintenance")
+        //            .Add("run")
+        //            .Add("--task")
+        //            .Add("gc")
+        //        )
+        //        .WithValidation(CommandResultValidation.None)
+        //        .ExecuteBufferedAsync();
+        //    Console.WriteLine($"{gitDir} - ExitCode:{gitMaintenance.ExitCode} Output:{gitMaintenance.StandardOutput}{Environment.NewLine}{gitMaintenance.StandardError}");
+        //}
         var result = await Cli.Wrap("git")
                 .WithWorkingDirectory(gitDir)
                 .WithArguments(args => args
@@ -113,6 +102,31 @@ async Task CliUpdateGit(string gitDir, int remaining = 0)
 
 }
 
+async Task CliUpdateRust()
+{
+    try
+    {
+        Console.ForegroundColor = colorize();
+        var result = await Cli.Wrap("rustup")
+                .WithArguments(args => args
+                    .Add("update")
+                    .Add("stable")
+                )
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteBufferedAsync();
+        Console.WriteLine($"ExitCode:{result.ExitCode} Output:{result.StandardOutput}{Environment.NewLine}{result.StandardError}");
+
+        Console.ResetColor();
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error: {ex}");
+        Console.ResetColor();
+    }
+
+}
+
 static ConsoleColor colorize()
 {
     List<ConsoleColor>? values = null;
@@ -124,22 +138,23 @@ static ConsoleColor colorize()
             ConsoleColor.Gray,
             ConsoleColor.Green,
             ConsoleColor.Magenta,
-            ConsoleColor.Red,
+            //ConsoleColor.Red,
             ConsoleColor.White,
             ConsoleColor.Yellow,
             ConsoleColor.DarkBlue,
+            ConsoleColor.Blue,
             ConsoleColor.DarkCyan,
             ConsoleColor.DarkGray,
             ConsoleColor.DarkGreen,
-            ConsoleColor.DarkMagenta,
+            //ConsoleColor.DarkMagenta,
             ConsoleColor.DarkRed,
             ConsoleColor.DarkYellow,
             ConsoleColor.Blue,
             ConsoleColor.Cyan,
             ConsoleColor.Gray,
             ConsoleColor.Green,
-            ConsoleColor.Magenta,
-            ConsoleColor.Red,
+            //ConsoleColor.Magenta,
+            //ConsoleColor.Red,
             ConsoleColor.White,
             ConsoleColor.Yellow,
         ];
